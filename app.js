546554a07ffba59a71a2a8e80774d8d0393672a5
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-02-23 23:52:42 -0300

    added homepage

--- app.js
+++ app.js
@@ -35,7 +35,11 @@ conn.authenticate()
 });
 
 app.get("/", (req, res) => {
-	res.render("index");
+
+	Article.findAll().then(articles => {
+		res.render("index", {articles: articles});
+	});
+	
 	// res.send("Bem vindo ao site");
 });
 
