cb2f4403db618278dfca19e4f7f9a79ac9ca64c9
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-08-02 22:00:28 -0300

    save in bd with hash password

--- user/UserController.js
+++ user/UserController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require('bcryptjs');
 
 router.get("/admin", (req, res) => {
     res.send("Listagem de usuários");
@@ -14,7 +15,20 @@ router.post("/create", (req, res) => {
     email = req.body.email;
     password = req.body.password;
 
-    res.json({email, password});
+    var salt = bcrypt.genSaltSync(10);
+    // console.log(salt)
+    var hash = bcrypt.hashSync(password, salt);
+    // console.log(hash)
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/")
+    }).catch((err) => {
+        console.log(err);
+        res.redirect("/")
+    });
 });
 
 module.exports = router;
